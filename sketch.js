aab344de5a7260b73833e02a15ad6b00a2933a90
josephmara12 <71110479+josephmara12@users.noreply.github.com>
2024-10-04 16:35:05 +1000

    basic working code

--- sketch.js
+++ sketch.js
@@ -1,29 +1,57 @@
 let container = document.querySelector('.container')
 container.style.display = 'none'
-let gridDimensions
-
+click = 0
 let genbutt = document.querySelector('#generate')
 let userInput = document.querySelector('#userInput')
 
-
 function generate_Grid() {
-    container.style.border = 'solid 2px black'
 
+    container.style.border = 'solid 2px black'
+    
     genbutt.addEventListener("click", () => {
-    gridDimensions = userInput.value
-    container.style.display = 'flex'
-        for (let i = 0; i < (gridDimensions*gridDimensions); i++) {
-            let cell = document.createElement('div');
-            let divDimensions = 600 / gridDimensions;           
-            cell.style.width = `${divDimensions}px`;
-            cell.style.height = `${divDimensions}px`;
-            cell.style.backgroundColor = 'red'  
-            container.appendChild(cell);
+        click += 1
+        let gridDimensions = userInput.value
+
+        if (click > 1) {
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+        }    
+
+
+        if (isNaN(gridDimensions)) {
+            alert('Please Enter a number')
         }
-    });
+        else if (gridDimensions > 100) {
+            alert('Please Enter a number lower than 100')
+        }
+        
+        else {
+
+        
+            container.style.display = 'flex'
+            for (let i = 0; i < (gridDimensions * gridDimensions); i++) {
+                let cell = document.createElement('div');
+                let divDimensions = 600 / gridDimensions;
+                cell.style.width = `${divDimensions}px`;
+                cell.style.height = `${divDimensions}px`;
+                //cell.style.border = 'solid black'
+                container.appendChild(cell);
+                cell.addEventListener("mouseover", () => {
+                    
+                cell.style.backgroundColor = 'red'
+
+                })  
+            }
+
+        }   
+    })
 }
 
+
+
+
+
+
+
 generate_Grid()
-
-
-
